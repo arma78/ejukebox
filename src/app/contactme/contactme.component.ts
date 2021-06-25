ada6b8272446b33b26be087d40592339b6174e05
Arma78 <arminrazic@hotmail.com>
2021-06-24 22:42:06 -0400

    First commit

---
+++ src/app/contactme/contactme.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactMeService } from '../Services/ContactMeService';
+import { NgForm, NgControl } from '@angular/forms';
+import { ContactMe } from '../models/contactme.model';
+import { CustomvalidationService } from '../Services/customvalidation.service';
+import { Router } from '@angular/router';
+import {
+  FormGroup,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AngularFireList } from '@angular/fire/database/database';
+@Component({
+  selector: 'app-contactme',
+  templateUrl: './contactme.component.html',
+  styleUrls: ['./contactme.component.scss'],
+  providers: [ContactMeService]
+})
+
+export class ContactmeComponent implements OnInit {
+  public contactForm: FormGroup;
+  submitted = false;
+  contactMeList: AngularFireList<any>;
+
+  contactMe: ContactMe = new ContactMe();
+
+  constructor(private contactMeService: ContactMeService,
+    // tslint:disable-next-line:align
+    private router: Router,
+    // tslint:disable-next-line:align
+    private formBuilder: FormBuilder,
+    // tslint:disable-next-line:align
+    private customValidator: CustomvalidationService) {
+  }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      phoneNumber: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      city: ['', [Validators.required]],
+      tattooType: ['', [Validators.required]],
+      sizeWidth: ['', [Validators.required]],
+      sizeHeight: ['', [Validators.required]],
+    }
+    );
+  }
+  // tslint:disable-next-line:typedef
+  get firstName() {
+    return this.contactForm.get('firstName');
+  }
+  // tslint:disable-next-line:typedef
+  get lastName() {
+    return this.contactForm.get('lastName');
+  }
+  // tslint:disable-next-line:typedef
+  get phoneNumber() {
+    return this.contactForm.get('phoneNumber');
+  }
+  // tslint:disable-next-line:typedef
+  get email() {
+    return this.contactForm.get('email');
+  }
+  // tslint:disable-next-line:typedef
+  get comments() {
+    return this.contactForm.get('comments');
+  }
+  // tslint:disable-next-line:typedef
+  get city() {
+    return this.contactForm.get('city');
+  }
+  // tslint:disable-next-line:typedef
+  get tattooType() {
+    return this.contactForm.get('tattooType');
+  }
+  // tslint:disable-next-line:typedef
+  get sizeWidth() {
+    return this.contactForm.get('sizeWidth');
+  }
+  // tslint:disable-next-line:typedef
+  get sizeHeight() {
+    return this.contactForm.get('sizeHeight');
+  }
+
+
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    this.contactMeService.create(this.contactMe);
+    this.router.navigate(['gallery']);
+    this.submitted = true;
+  }
+
+}
+
+
