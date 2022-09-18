cd4d4459f43cadb14370584a9fbb676e8f305ba2
Armin Razic <armin.razic@axessgroup.com>
2022-09-18 14:03:29 +0200

    Update

--- src/app/ejuboxplayer/ejuboxplayer.component.ts
+++ src/app/ejuboxplayer/ejuboxplayer.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, OnChanges, ViewChild} from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild } from '@angular/core';
 import { SongListService } from '../Services/songlist.service';
-import { SharedServiceService} from "../Services/shared-service.service";
+import { SharedServiceService } from "../Services/shared-service.service";
 import { SelectService } from '../Services/select.service';
-import {Track,AudioPlayerComponent,NgxAudioPlayerModule} from 'ngx-audio-player';
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs';
+import { Track, AudioPlayerComponent, NgxAudioPlayerModule } from 'ngx-audio-player';
 
 @Component({
   selector: 'app-ejuboxplayer',
@@ -15,87 +13,67 @@ import { BehaviorSubject } from 'rxjs';
 export class EjuboxplayerComponent implements OnInit, OnChanges {
   fileUploads = [];
   currentTime: any;
-  message:string;
+  message: string;
 
-  constructor(private apc: AudioPlayerComponent,private sharedService:  SharedServiceService, private songlistservice: SongListService, private selectService: SelectService)
-  {
+  constructor(private apc: AudioPlayerComponent, private sharedService: SharedServiceService, private songlistservice: SongListService, private selectService: SelectService) {
   }
   @ViewChild('player', { static: false })
-
   ngm: NgxAudioPlayerModule
   msaapPlaylist: Track[] = [];
-
-
-  // tslint:disable-next-line:typedef
   ngOnInit() {
     this.sharedService.removeTrackerService();
     this.getAllList();
-
-   }
+  }
   // tslint:disable-next-line:typedef
   ngOnChanges() {
-    console.log('Jupi');
-
-   }
-
-
-
-
-
-
+  }
   onEnded(event) {
-
+    this.RequestedSongs();
     var list = "";
-       setTimeout(() => {
+    setTimeout(() => {
       list = document.getElementsByTagName("marquee")[0].innerHTML;
-             this.sharedService.currentlyplayTracker(list);
+      this.sharedService.currentlyplayTracker(list);
 
     }, 2045);
 
   }
-
-  // tslint:disable-next-line:typedef
   getAllList() {
-      this.songlistservice.getCurrentPlayList().snapshotChanges().map(changes => {
+    this.songlistservice.getCurrentPlayList().snapshotChanges().map(changes => {
       return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
     }).subscribe((fileUploads: any) => {
       this.fileUploads = fileUploads;
       this.fileUploads = this.fileUploads.sort((a, b) => a.sequence - b.sequence);
     });
   }
-
-
-
-
-    Test() {
-
-
-
-    }
-
-
-
-
-
-  LoadPlayList() {
-    this.msaapPlaylist = [];
-
-    for(var j = this.fileUploads.length -1; j >= 0 ; j--){
-      if(this.fileUploads[j].currentplaylist == 'false'){
-        this.fileUploads.splice(j, 1);
-      }
-  }
-
-    for (var i = 0; i < this.fileUploads.length; i++) {
+  RequestedSongs() {
+    const diff1: Track[] = this.fileUploads.filter(item => this.msaapPlaylist.indexOf(item) < 0);
+    const diff2: Track[] = this.msaapPlaylist.filter(item => this.fileUploads.indexOf(item) < 0);
+    const diff = diff1.length - diff2.length;
+    if (diff > 0) {
+      for (let i = 0; i < diff; i++) {
         this.msaapPlaylist.push({
           title: this.fileUploads[i].id,
           link: this.fileUploads[i].url,
           artist: this.fileUploads[i].artist,
         });
+      }
     }
   }
-
-
+  LoadPlayList() {
+    this.msaapPlaylist = [];
+    for (var j = this.fileUploads.length - 1; j >= 0; j--) {
+      if (this.fileUploads[j].currentplaylist == 'false') {
+        this.fileUploads.splice(j, 1);
+      }
+    }
+    for (var i = 0; i < this.fileUploads.length; i++) {
+      this.msaapPlaylist.push({
+        title: this.fileUploads[i].id,
+        link: this.fileUploads[i].url,
+        artist: this.fileUploads[i].artist,
+      });
+    }
+  }
   msaapDisplayTitle = true;
   msaapDisplayPlayList = true;
   msaapPageSizeOptions = [6, 4, 6];
@@ -104,12 +82,6 @@ export class EjuboxplayerComponent implements OnInit, OnChanges {
   msaapDisplayArtist = true;
   msaapDisplayDuration = false;
   msaapDisablePositionSlider = false;
-
-
-
-
-
-
 
 
 
